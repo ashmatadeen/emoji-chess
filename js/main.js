cb7de784c2ca3890f0e4b20afdfdfa3dc0c64afa
Ash Matadeen <ash.matadeen@gmail.com>
2018-02-26 21:28:06 +0000

    board colouring

--- js/main.js
+++ js/main.js
@@ -6,14 +6,20 @@ renderBoard();
 
 function renderBoard() {
     var container = document.getElementById('chess-board');
-    var the_cell;
+    var colour_toggler = true;
+    var the_cell, colour;
     // Reverse for AN format: https://en.wikipedia.org/wiki/Algebraic_notation_(chess)
     row = row.reverse();
     row.forEach(function renderRow(i) {
         col.forEach(function renderCell(j) {
+            colour = colour_toggler
+                ? 'white'
+                : 'black';
             the_cell = document.createElement('div');
-            the_cell.className = 'box ' + i + j;
+            the_cell.className = 'box ' + colour + ' ' + i + j;
             container.appendChild(the_cell);
+            colour_toggler = !colour_toggler;
         });
+        colour_toggler = !colour_toggler;
     });
 }
